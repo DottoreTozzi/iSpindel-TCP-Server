fb0f299990048ad6f9746cb946b51458a6641ed2
Stephan Schreiber <stephan@sschreiber.de>
2017-09-27 01:31:20 +0200

    Update Scripts added

---
+++ update-raspi.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo Updating from GIT...
+git pull
+echo Installing Server Script...
+sudo cp ./iSpindle.py /usr/local/bin/
+echo Updating Raspbian Package List...
+sudo apt-get update
+echo Updating Raspbian Packages...
+sudo apt-get dist-upgrade
+echo Cleaning Up...
+sudo apt-get autoremove
+sudo apt-get autoclean
+echo Done.
+read -p 'Reboot Now? (y/n): ' -e reboot
+if [[ "$reboot" = "y" ]]; then
+	sudo shutdown -r now
+fi
---
+++ update.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo Updating from GIT...
+git pull
+echo Installing Server Script...
+sudo cp ./iSpindle.py /usr/local/bin/
+echo Done.
