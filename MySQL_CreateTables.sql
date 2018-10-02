61e4104aa21e35ac6f8afce94d6c8dda9cbc100f
Stephan Schreiber <stephan@sschreiber.de>
2018-10-02 05:57:33 +0200

    Update MySQL_CreateTables.sql

--- MySQL_CreateTables.sql
+++ MySQL_CreateTables.sql
@@ -24,3 +24,13 @@ CREATE TABLE `Calibration` (
 ) 
 ENGINE=InnoDB DEFAULT CHARSET=ascii 
 COLLATE=ascii_bin COMMENT='iSpindle Calibration Data';
+
+CREATE TABLE `Config` (
+	`ID` int NOT NULL,
+	`Interval` int NOT NULL,
+	`Token` varchar(64) NOT NULL,
+	`Polynomial` varchar(64) NOT NULL,
+        `Sent` boolean NOT NULL,
+	PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=ascii COLLATE=ascii_bin COMMENT='iSpindle Config Data';
+
