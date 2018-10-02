8f14f2ecc73628c6dc0a580a17fa48c706652ed8
Stephan Schreiber <stephan@sschreiber.de>
2018-10-02 05:05:29 +0200

    Added CBP3 support. Added Remote Config via JSON Response to TCP connection.

--- iSpindle.py
+++ iSpindle.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python2.7
 
+#
+# Version: 1.6.0
+# iSpindel Remote Config via JSON TCP response implemented
+# Added CraftBeerPi 3.0 Support (thanks to jlanger)
+#
 # Version: 1.5.0
-# Added: Brewpiless support
+# Added: Brewpiless support (thanks to ollinator2000)
 # Testing: Return JSON with TCP Reply
 #
 # Version: 1.4.1
 # New: Added new data fields for Interval and WiFi reception (RSSI) for Firmware 5.8 and later
 # Chg: TimeStamp in CSV now in first column
-# New:  Added option to forward to fermentrack http://www.fermentrack.com/
+# New: Added option to forward to fermentrack http://www.fermentrack.com/ (thanks to Th3ju)
 
 # Version: 1.3.3
 # New:  Added config parameter to use token field in iSpindle config as Ubidots token
@@ -44,7 +49,7 @@ NEWLINE = '\r\n'  # newline (\r\n for windows clients)
 DATETIME = 1  # Leave this at 1 to include Excel compatible timestamp in CSV
 
 # MySQL
-SQL = 0  # 1 to enable output to MySQL database
+SQL = 1  # 1 to enable output to MySQL database
 SQL_HOST = '127.0.0.1'  # Database host name (default: localhost - 127.0.0.1 loopback interface)
 SQL_DB = 'iSpindle'  # Database name
 SQL_TABLE = 'Data'  # Table name
@@ -70,24 +75,71 @@ FERMENTRACKADDR = '192.168.10.164'
 FERMENTRACK_TOKEN = 'mytoken'
 FERMENTRACKPORT = 80
 
-# ADVANCED
-ENABLE_ADDCOLS = 0  # Enable dynamic columns (do not use this unless you're a developer)
-RETURN_JSON = 1
-RETURN_JSON_EXTENDED = 1
-
 # BREWPILESS
 BREWPILESS = 0
 BREWPILESSADDR = '192.168.0.102:80'
+
+# Forward to CraftBeerPi3 iSpindel Addon
+CRAFTBEERPI3 = 0
+CRAFTBEERPI3ADDR = 'localhost:5000'
+# if this is true the raw angle will be sent to CBPI3 instead of
+# the gravity value. Use this if you want to configure the
+# polynome from within CBPI3.
+# Otherwise leave this 0 and just use "tilt" in CBPI3
+CRAFTBEERPI3_SEND_ANGLE = 0
+
+# iSpindle Remote Config?
+# If this is enabled, we'll send iSpindle config JSON as TCP reply.
+# Before using this, make sure your database is up-to-date. See README and INSTALL.
+# This feature is still in testing but should already work reliably.
+REMOTECONFIG = 0
+
+# ADVANCED
+ENABLE_ADDCOLS = 0  # Enable dynamic columns (do not use this unless you're a developer)
 
 # CONFIG End
 
 ACK = chr(6)  # ASCII ACK (Acknowledge)
 NAK = chr(21)  # ASCII NAK (Not Acknowledged)
-BUFF = 1024  # Buffer Size (greatly exaggerated for now)
+BUFF = 256  # Buffer Size
+
+# iSpindel Config Param Arrays
+lConfigIDs = []
+dInterval = {}
+dToken = {}
+dPoly = {}
 
 
 def dbgprint(s):
     if DEBUG: print(str(s))
+
+
+def readConfig():
+    if REMOTECONFIG:
+        dbgprint('Preparing iSpindel config data...')
+        try:
+            import mysql.connector
+            cnx = mysql.connector.connect(user=SQL_USER, password=SQL_PASSWORD, host=SQL_HOST, database=SQL_DB)
+            cur = cnx.cursor()
+            cur.execute("SELECT * FROM Config WHERE NOT Sent;")
+            ispindles = cur.fetchall()
+            del lConfigIDs[:] #ConfigIDs.clear()
+            dInterval.clear()
+            dToken.clear()
+            dPoly.clear()
+            for i in ispindles:
+                id = i[0]  # ID
+                sId = str(id)
+                lConfigIDs.append(id)
+                dInterval[sId] = i[1]   # Interval
+                dToken[sId] = str(i[2]) # Token
+                dPoly[sId] = str(i[3])  # Polynomial
+            cur.close()
+            cnx.close()
+            dbgprint('Config data: Done. ' + str(len(lConfigIDs)) + " config change(s) to submit.")
+        except Exception as e:
+            dbgprint('Error: ' + str(e))
+            dbgprint('Did you properly add the "Config" table to your database?')
 
 
 def handler(clientsock, addr):
@@ -103,6 +155,7 @@ def handler(clientsock, addr):
     interval = 0
     rssi = 0
     timestart = time.clock()
+    config_sent = 0
 
     while 1:
         data = clientsock.recv(BUFF)
@@ -140,20 +193,37 @@ def handler(clientsock, addr):
                     # older firmwares < 5.4 or field not filled in
                     user_token = '*'
                 # looks like everything went well :)
-                # Testing: Return Interval as JSON?
-                reply = ''
-                if RETURN_JSON:
-		    if RETURN_JSON_EXTENDED:
-		        reply = '{ "interval" : ' + str(interval) + ',"name":"iSpindel01","vfact":191.80,"token":"*dummy","polyn":"0.002378287*tilt^2+0.327188119*tilt-8.88"}'
-		    else:
-                        reply = '{ "interval" : ' + str(interval) + ' }'
-                    clientsock.send(reply)
+                #
+                # Should we reply with a config JSON?
+                #
+                if REMOTECONFIG:
+                    resp = ACK
+                    try:
+                        if spindle_id in lConfigIDs:
+                            sId = str(spindle_id)
+                            jresp = {}
+                            if dInterval[sId]:
+                                jresp["interval"] = dInterval[sId]
+                            if dToken[sId]:
+                                jresp["token"] = dToken[sId]
+                            if dPoly[sId]:
+                                jresp["polynomial"] = dPoly[sId]
+                            resp = json.dumps(jresp)
+                            dbgprint(repr(addr) + ' JSON Response: ' + resp)
+                            config_sent = 1
+                        else:
+                            dbgprint(repr(addr) + ' No unsent data for iSpindel "' + spindle_name + '". Sending ACK.')
+                    except Exception as e:
+                        dbgprint(repr(addr) + " Can't send config response. Something went wrong:")
+                        dbgprint(repr(addr) + " Error: " + str(e))
+                        dbgprint(repr(addr) + " Sending ACK.")
+                    clientsock.send(resp)
                 else:
-                    reply = 'ACK'
                     clientsock.send(ACK)
+                    dbgprint(repr(addr) + ' Sent ACK.')
+                #
                 dbgprint(repr(addr) + ' Time elapsed: ' + str(time.clock() - timestart))
-                dbgprint(repr(addr) + ' ' + spindle_name + ' (ID:' + str(spindle_id) + ') : Data received OK.')
-                dbgprint(repr(addr) + ' Reply: ' + reply)
+                dbgprint(repr(addr) + ' ' + spindle_name + ' (ID:' + str(spindle_id) + ') : Data Transfer OK.')
                 success = 1
                 break  # close connection
         except Exception as e:
@@ -165,6 +235,18 @@ def handler(clientsock, addr):
             break  # close connection server side after non-success
     clientsock.close()
     dbgprint(repr(addr) + " - closed connection")  # log on console
+
+    if config_sent:
+        # update sent status in config table
+        import mysql.connector
+        dbgprint(repr(addr) + ' - marking db config data as sent.')
+        cnx = mysql.connector.connect(user=SQL_USER, password=SQL_PASSWORD, host=SQL_HOST, database=SQL_DB)
+        cur = cnx.cursor()
+        sql = 'UPDATE Config SET Sent=True WHERE ID=' + str(spindle_id) + ';'
+        cur.execute(sql)
+        cnx.commit()
+        cur.close()
+        cnx.close()
 
     if success:
         # We have the complete spindle data now, so let's make it available
@@ -257,8 +339,8 @@ def handler(clientsock, addr):
                 valuestr = ', '.join(['%s' for x in valuelist])
                 add_sql = 'INSERT INTO Data (' + fieldstr + ')'
                 add_sql += ' VALUES (' + valuestr + ')'
-                dbgprint(add_sql)
-                dbgprint(valuelist)
+                #dbgprint(add_sql)
+                #dbgprint(valuelist)
                 cur.execute(add_sql, valuelist)
                 cnx.commit()
                 cur.close()
@@ -281,6 +363,28 @@ def handler(clientsock, addr):
                 out = json.dumps(outdata)
                 dbgprint(repr(addr) + ' - sending: ' + out)
                 url = 'http://' + BREWPILESSADDR + '/gravity'
+                req = urllib2.Request(url)
+                req.add_header('Content-Type', 'application/json')
+                req.add_header('User-Agent', spindle_name)
+                response = urllib2.urlopen(req, out)
+                dbgprint(repr(addr) + ' - received: ' + response.read())
+
+            except Exception as e:
+                dbgprint(repr(addr) + ' Error while forwarding to URL ' + url + ' : ' + str(e))
+
+        if CRAFTBEERPI3:
+            try:
+                dbgprint(repr(addr) + ' - forwarding to CraftBeerPi3 at http://' + CRAFTBEERPI3ADDR)
+                import urllib2
+                outdata = {
+                    'name': spindle_name,
+                    'angle': angle if CRAFTBEERPI3_SEND_ANGLE else gravity,
+                    'temperature': temperature,
+                    'battery': battery,
+                }
+                out = json.dumps(outdata)
+                dbgprint(repr(addr) + ' - sending: ' + out)
+                url = 'http://' + CRAFTBEERPI3ADDR + '/api/hydrometer/v1/data'
                 req = urllib2.Request(url)
                 req.add_header('Content-Type', 'application/json')
                 req.add_header('User-Agent', spindle_name)
@@ -346,7 +450,6 @@ def handler(clientsock, addr):
                     dbgprint(repr(addr) + ' - received NAK - Not OK...')
                 else:
                     dbgprint(repr(addr) + ' - received: ' + rcv)
-
             except Exception as e:
                 dbgprint(repr(addr) + ' Error while forwarding to ' + FORWARDADDR + ': ' + str(e))
 
@@ -381,6 +484,8 @@ def handler(clientsock, addr):
             except Exception as e:
                 dbgprint(repr(addr) + ' Fermentrack Error: ' + str(e))
 
+        readConfig()
+
 
 def main():
     ADDR = (HOST, PORT)
@@ -388,12 +493,12 @@ def main():
     serversock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     serversock.bind(ADDR)
     serversock.listen(5)
+    readConfig()
     while 1:
         dbgprint('waiting for connection... listening on port: ' + str(PORT))
         clientsock, addr = serversock.accept()
         dbgprint('...connected from: ' + str(addr))
         thread.start_new_thread(handler, (clientsock, addr))
-
 
 if __name__ == "__main__":
     main()
